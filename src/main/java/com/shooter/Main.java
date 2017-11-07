39cb06fc6bc1ccec9e6fc4299c82f9d22fd21ba9
vladtkachuk <tkachuk.v26@gmail.com>
2017-11-07 16:35:03 +0200

    connection message added

--- src/main/java/com/shooter/Main.java
+++ src/main/java/com/shooter/Main.java
@@ -10,18 +10,18 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         int port = 4444;
         String host = null;
-        if (args.length>0) {
+        if (args.length > 0) {
             host = args[0];
-            System.out.println("Connecting to: "+host);
-        }else {
-            System.out.println("No ip given -> setting up a server at port: "+port);
-
+            System.out.println("Connecting to: " + host);
+        } else {
+            System.out.println("No ip given -> setting up a server at port: " + port);
         }
 
-        if (host==null){
+        if (host == null) {
             try {
                 ServerSocket ss = new ServerSocket(port);
                 Socket socket = ss.accept();
+                System.out.println("New connection " + socket.getInetAddress().toString());
                 socket.getOutputStream().write("Hello".getBytes());
                 socket.getOutputStream().flush();
 
@@ -29,11 +29,9 @@ public class Main {
                 e.printStackTrace();
             }
         } else {
-
-
             try {
-                final Socket socket = new Socket(host,port);
-                Thread t = new Thread(){
+                final Socket socket = new Socket(host, port);
+                Thread t = new Thread() {
                     @Override
                     public void run() {
                         try {
@@ -49,7 +47,6 @@ public class Main {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
 
 
         }
